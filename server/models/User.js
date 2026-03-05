eea3d56e32d9802ac793ff7c7ca1ca8f2a4aac73
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2026-03-05 15:23:39 +0300

    default username

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     first_name : {type: String, required: true},
     last_name: {type: String, required: true},
-    username: {type:String},
+    username: {type:String, default: "SamdigitalX"},
     email: {type: String, required: true, unique: true},
     phone: String,
     role: { type: String, enum: ["admin"], default: "admin"},
